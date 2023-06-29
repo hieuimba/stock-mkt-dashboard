4a73f5782d736ca9a7627031ee8e8b0d6f5f89a5
Hieu Ngo <89481020+hieuimba@users.noreply.github.com>
2023-06-29 00:13:35 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,7 +37,7 @@ def string_to_array(row):
 #         ticker_dict[ticker].append(close_price)
 #     return ticker_dict
 
-st.sidebar.text("testing!")
+st.sidebar.text("some texts")
 
 
 date_timestamp = query("SELECT MAX([DATE]) AS Date FROM analytics.TodaySnapshot")["Date"][0]
@@ -68,30 +68,57 @@ for i, signal in enumerate(signals):
     df_stock_rank_highest = df_stock_rank_highest[['Ticker','Value','Prices','Last','Change','Volume']]
     df_stock_rank_lowest = df_stock_rank_lowest[['Ticker','Value','Prices','Last','Change','Volume']]
 
+    value_count = df_stock_his["BinCount"].sum()
     if i == 0:
         category_order = {"Bin":["> 100", "87.5 - 100", "75 - 87.5", "62.5 - 75", "50 - 62.5", "37.5 - 50", "25 - 37.5", "12.5 - 25", "0 - 12.5", "< 0"]}
         color_seq =  ["#22c55e"] * 5 + ["#ef4444"] * 5 
         header = "Year Range"
         col_name = "52 Week Range"
+
+        metric_1_name = 'New Highs'
+        metric_1_value = df_stock_his.loc[df_stock_his['Bin'] == '> 100', 'BinCount'].values[0]
+        metric_1_value_formatted = f'{metric_1_value} ({round(metric_1_value/value_count*100,1)} %)'
+        metric_2_name = 'New Lows'
+        metric_2_value = df_stock_his.loc[df_stock_his['Bin'] == '< 0', 'BinCount'].values[0]
+        metric_2_value_formatted = f'{metric_2_value} ({round(metric_2_value/value_count*100,1)} %)'
     elif i == 1:
         category_order = {"Bin":["> 100", "87.5 - 100", "75 - 87.5", "62.5 - 75", "50 - 62.5", "37.5 - 50", "25 - 37.5", "12.5 - 25", "0 - 12.5", "< 0"]}
         color_seq = ["#22c55e"] * 5 + ["#ef4444"] * 5 
         header = "Month Range"
-        col_name = "Kelner Channel Position"
+        col_name = "KC Position"
+
+        metric_1_name = 'Strong Bullish Move'
+        metric_1_value = df_stock_his.loc[df_stock_his['Bin'] == '> 100', 'BinCount'].values[0]
+        metric_1_value_formatted = f'{metric_1_value} ({round(metric_1_value/value_count*100,1)} %)'
+        metric_2_name = 'Strong Bearish Move'
+        metric_2_value = df_stock_his.loc[df_stock_his['Bin'] == '< 0', 'BinCount'].values[0]
+        metric_2_value_formatted = f'{metric_2_value} ({round(metric_2_value/value_count*100,1)} %)'
+
     elif i == 2:
         category_order = {"Bin":["> 4", "3 - 4", "2 - 3", "1 - 2", "0 - 1", "-1 - 0", "-2 - -1", "-3 - -2", "-4 - -3", "< -4"]}
         color_seq = ["#22c55e"] * 5 +  ["#ef4444"] * 5 
         header = "Relative Return"
         col_name = "Sigma Spike"
+
+        metric_1_name = 'Strong Bullish SS'
+        metric_1_value = df_stock_his.loc[df_stock_his['Bin'] == '> 4', 'BinCount'].values[0]
+        metric_1_value_formatted = f'{metric_1_value} ({round(metric_1_value/value_count*100,1)} %)'
+        metric_2_name = 'Strong Bearish SS'
+        metric_2_value = df_stock_his.loc[df_stock_his['Bin'] == '< -4', 'BinCount'].values[0]
+        metric_2_value_formatted = f'{metric_2_value} ({round(metric_2_value/value_count*100,1)} %)'
     elif i == 3:
         category_order = {"Bin":["> 5", "4 - 5", "3 - 4", "2 - 3", "1 - 2", "0 - 1"]}
         color_seq = ["#0ea5e9"]
         header = "Relative Volume"
         col_name = "Relative Volume"
 
+        metric_1_name = 'Strong Volume'
+        metric_1_value = df_stock_his.loc[df_stock_his['Bin'] == '> 5', 'BinCount'].values[0]
+        metric_1_value_formatted = f'{metric_1_value} ({round(metric_1_value/value_count*100,1)} %)'
+
     st.divider()
     
-    one, two = st.columns([1,2])
+    one, two, three = st.columns([1.5,0.75,2.5])
 
 
     with one:
@@ -106,8 +133,14 @@ for i, signal in enumerate(signals):
                            labels={'Bin':'Distribution', 'BinCount':'Count'}
                            )
         st.plotly_chart(histogram,use_container_width=True)
-
     with two:
+        st.title(' ')
+        st.title(' ')
+        st.title(' ')
+        st.metric(value=metric_1_value_formatted, label = metric_1_name)
+        if i != 3:
+            st.metric(value=metric_2_value_formatted, label = metric_2_name)
+    with three:
         tab_one, tab_two = st.tabs(["Highest","Lowest"])
         with tab_one:
             df_stock_rank_highest = df_stock_rank_highest.rename(columns = {'Value':col_name})
@@ -118,6 +151,7 @@ for i, signal in enumerate(signals):
                             "Change":st.column_config.NumberColumn("Change",format="%.2f %%")
                             }, 
                            hide_index=True, 
+                           disabled= True,
                            use_container_width=True)
         with tab_two:
             df_stock_rank_lowest = df_stock_rank_lowest.rename(columns = {'Value':col_name})
@@ -128,5 +162,6 @@ for i, signal in enumerate(signals):
                             "Change":st.column_config.NumberColumn("Change",format="%.2f %%")
                             }, 
                            hide_index=True, 
+                           disabled= True,
                            use_container_width=True)
 
