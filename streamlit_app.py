4c7d06da1d19ce336edeb70e0b55278a86e62883
Hieu Ngo <89481020+hieuimba@users.noreply.github.com>
2023-07-02 12:41:56 -0500

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,9 @@ import json
 import pandas as pd
 import plotly.express as px
 import numpy as np
+
 st.set_page_config(layout="wide")
+
 
 # Define the URL and payload for the POST request
 url =  st.secrets["DB_URL"]
@@ -20,35 +22,11 @@ def string_to_array(row):
     values_list = [float(value) for value in row.split(",")]
     return np.array(values_list)
 
-# @st.cache_data(ttl = 3600)
-# def query_prices(tickers_list):
-#     tickers_string = ','.join(tickers_list)
-#     query = f"EXEC GetClosePrices @Tickers = '{tickers_string}'"
-#     request = {"query": query}
-#     response = json.loads(requests.post(url, json=request).content.decode('utf-8'))
-#     ticker_dict = {}
-#     for entry in response:
-#         ticker = entry['Ticker']
-#         close_price = entry['close']
-        
-#         if ticker not in ticker_dict:
-#             ticker_dict[ticker] = []
-        
-#         ticker_dict[ticker].append(close_price)
-#     return ticker_dict
 
-st.sidebar.text("some texts")
+mkt_his = query("select * from visual.StockHistogram")
+mkt_rank = query("SELECT * from visual.StockRanking")
 
-
-date_timestamp = query("SELECT MAX([DATE]) AS Date FROM analytics.TodaySnapshot")["Date"][0]
-unique_tickers = query("SELECT COUNT(DISTINCT ticker) AS Count FROM analytics.TodaySnapshot Where SecType = 'Stock'")["Count"][0]
-st.caption(f"Looking at {unique_tickers} active tickers from NASDAQ, AMEX, and NYSE. Data as of {date_timestamp}")
-
-
-mkt_his = query("select * from visual.MktHistogram")
-mkt_rank = query("SELECT * from visual.MktRanking")
-
-signals = mkt_his['Signal'].unique()
+signals = ['52WkRange', 'KCPos', 'SigmaSpike','RVol']
 
 stock_his = mkt_his[mkt_his["SecType"] == "Stock"]
 stock_rank = mkt_rank[mkt_rank["SecType"] == "Stock"]
@@ -65,8 +43,8 @@ for i, signal in enumerate(signals):
 
     df_stock_rank_highest = df_stock_rank[df_stock_rank["RankGroup"]=="Highest"]
     df_stock_rank_lowest = df_stock_rank[df_stock_rank["RankGroup"]=="Lowest"]
-    df_stock_rank_highest = df_stock_rank_highest[['Ticker','Value','Prices','Last','Change','Volume']]
-    df_stock_rank_lowest = df_stock_rank_lowest[['Ticker','Value','Prices','Last','Change','Volume']]
+    df_stock_rank_highest = df_stock_rank_highest[['Ticker','Value','Prices','Last','Change','Volume','Sector']]
+    df_stock_rank_lowest = df_stock_rank_lowest[['Ticker','Value','Prices','Last','Change','Volume','Sector']]
 
     value_count = df_stock_his["BinCount"].sum()
     if i == 0:
@@ -116,9 +94,8 @@ for i, signal in enumerate(signals):
         metric_1_value = df_stock_his.loc[df_stock_his['Bin'] == '> 5', 'BinCount'].values[0]
         metric_1_value_formatted = f'{metric_1_value} ({round(metric_1_value/value_count*100,1)} %)'
 
-    st.divider()
     
-    one, two, three = st.columns([1.5,0.75,2.5])
+    one, two = st.columns([1,2])
 
 
     with one:
@@ -130,24 +107,19 @@ for i, signal in enumerate(signals):
                            color="Bin", 
                            color_discrete_sequence=color_seq,
                            height =400,
-                           labels={'Bin':'Distribution', 'BinCount':'Count'}
+                           labels={'Bin':'Distribution', 'BinCount':'Count'},
+                           text_auto=True
                            )
+        histogram.update_layout(showlegend=False)
         st.plotly_chart(histogram,use_container_width=True)
     with two:
-        st.title(' ')
-        st.title(' ')
-        st.title(' ')
-        st.metric(value=metric_1_value_formatted, label = metric_1_name)
-        if i != 3:
-            st.metric(value=metric_2_value_formatted, label = metric_2_name)
-    with three:
         tab_one, tab_two = st.tabs(["Highest","Lowest"])
         with tab_one:
             df_stock_rank_highest = df_stock_rank_highest.rename(columns = {'Value':col_name})
             st.data_editor(df_stock_rank_highest, 
                            height = 388,
                            column_config=
-                           {"Prices":st.column_config.LineChartColumn("Price",width="small"),
+                           {"Prices":st.column_config.LineChartColumn("21 Day Chart",width="small"),
                             "Change":st.column_config.NumberColumn("Change",format="%.2f %%")
                             }, 
                            hide_index=True, 
@@ -158,10 +130,14 @@ for i, signal in enumerate(signals):
             st.data_editor(df_stock_rank_lowest, 
                            height = 400,
                            column_config=
-                           {"Prices":st.column_config.LineChartColumn("Price",width="small"),
+                           {"Prices":st.column_config.LineChartColumn("21 Day Chart",width="small"),
                             "Change":st.column_config.NumberColumn("Change",format="%.2f %%")
                             }, 
                            hide_index=True, 
                            disabled= True,
                            use_container_width=True)
 
+
+date_timestamp = query("SELECT MAX([DATE]) AS Date FROM analytics.TodaySnapshot")["Date"][0]
+unique_tickers = query("SELECT COUNT(DISTINCT ticker) AS Count FROM analytics.TodaySnapshot Where SecType = 'Stock'")["Count"][0]
+st.caption(f"Looking at {unique_tickers} active tickers from NASDAQ, AMEX, and NYSE. Data as of {date_timestamp}")
