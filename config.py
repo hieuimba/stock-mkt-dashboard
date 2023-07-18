c3bb5d68b26b300bedd40373e556066415511ed7
hieuimba <hieuimba@gmail.com>
2023-07-17 22:54:47 -0500

    clean up code

--- config.py
+++ config.py
@@ -2,7 +2,8 @@ import streamlit as st
 import json
 import pandas as pd
 import requests
-
+from typing import List
+import re
 
 # color settings:
 green = "#22c55e"
@@ -12,24 +13,31 @@ yellow = "#f59e0b"
 white = "#fafafa"
 darkgrey = "#171717"
 
+
 # Function to query data
-@st.cache_data(ttl = 3600)
-def query(query):
-    url =  st.secrets["DB_URL"]
+def query(query: str) -> pd.DataFrame:
+    url = st.secrets["DB_URL"]
     request = {"query": query}
-    response = json.loads(requests.post(url, json=request).content.decode('utf-8'))
+    response = json.loads(requests.post(url, json=request).content.decode("utf-8"))
     return pd.DataFrame(response)
 
+
 # Function to convert string to array of floats
-def string_to_array(row):
+def string_to_array(row: str) -> List[float]:
     values_list = [float(value) for value in row.split(",")]
     return values_list
 
 
 # Function to apply positive & negative color formatting
-def format_positive_negative_cell_color(val):
+def format_positive_negative_cell_color(val: float) -> str:
     color = red if val < 0 else green
-    return f'color: {color}'
+    return f"color: {color}"
+
+
+# Function to add space between capital letters
+def add_space(match: re.Match) -> str:
+    return " " + match.group(0)
+
 
 # page settings:
 condensed_page_style = """
@@ -39,62 +47,52 @@ condensed_page_style = """
 """
 
 # stocks histogram bins settings:
-signals_sorted = ['52WkRange', 'KCPos', 'SigmaSpike','RVol']
-yr_range_bins_sorted = ["> 100", "87.5 - 100", "75 - 87.5", "62.5 - 75", "50 - 62.5", "37.5 - 50", "25 - 37.5", "12.5 - 25", "0 - 12.5", "< 0"]
-sigma_spike_bins_sorted = ["> 4", "3 - 4", "2 - 3", "1 - 2", "0 - 1", "-1 - 0", "-2 - -1", "-3 - -2", "-4 - -3", "< -4"]
+signals_sorted = ["52WkRange", "KCPos", "SigmaSpike", "RVol"]
+yr_range_bins_sorted = [
+    "> 100",
+    "87.5 - 100",
+    "75 - 87.5",
+    "62.5 - 75",
+    "50 - 62.5",
+    "37.5 - 50",
+    "25 - 37.5",
+    "12.5 - 25",
+    "0 - 12.5",
+    "< 0",
+]
+sigma_spike_bins_sorted = [
+    "> 4",
+    "3 - 4",
+    "2 - 3",
+    "1 - 2",
+    "0 - 1",
+    "-1 - 0",
+    "-2 - -1",
+    "-3 - -2",
+    "-4 - -3",
+    "< -4",
+]
 rvol_bins_sorted = ["> 5", "4 - 5", "3 - 4", "2 - 3", "1 - 2", "0 - 1"]
 
 # queries settings:
 timestamp_query = "SELECT MAX([DATE]) AS Date FROM analytics.TodaySnapshot"
 unique_tickers_query = "SELECT COUNT(DISTINCT ticker) AS Count FROM analytics.TodaySnapshot Where SecType = 'Stock'"
 
-index_prices_query = "select  P.Ticker, T.Name,[Date], [Open], [High], [Low], [Close],[Return], Volume \
-    from raw.Prices P Left Join raw.Tickers T on P.Ticker = T.Ticker \
-        where T.SecType = 'Index'\
-            AND [Date] >= DATEADD(month, -2, GETDATE())"
-index_table_query = "select T.Name, S.Ticker, S.[Return] as 'OneDayReturn', R.OneWeekReturn as 'OneWeekReturn',\
-      R.OneMonthReturn as 'OneMonthReturn', R.OneQuarterReturn as 'OneQuarterReturn'\
-        , R.OneYearReturn as 'OneYearReturn', S.SigmaSpike, S.KCPos/100 as 'MonthRange', S.[C%52WkRange]/100 as 'YearRange'\
-    from analytics.TodaySnapshot S\
-    left join raw.Tickers T on T.Ticker = S.Ticker \
-    left join analytics.AggReturns R on R.Ticker  = S.Ticker\
-        where S.SecType = 'Index'"
+stock_his_query = "SELECT * from visual.StockHistogram"
+stock_ranking_query = "SELECT * from visual.StockRanking"
+
+index_prices_query = "select * from visual.CandlestickChart where SecType = 'Index'"
+index_table_query = "select * from visual.PerformanceTable where SecType = 'Index'"
+
 one_day_return_query = "SELECT S.Ticker, S.SecType, S.SigmaSpike, T.ShortName FROM analytics.TodaySnapShot S \
     Left Join config.TickerShortNames T on T.Ticker=S.Ticker\
         WHERE S.SecType = 'ETF'"
 
-etf_prices_query = "select  P.Ticker, T.ShortName as 'Name',[Date], [Open], [High], [Low], [Close],[Return], Volume \
-    from raw.Prices P Left Join config.TickerShortNames T on P.Ticker = T.Ticker \
-        where T.SecType = 'ETF'\
-            AND [Date] >= DATEADD(month, -2, GETDATE())\
-			AND P.Ticker LIKE 'XL%'\
-                ORDER BY Ticker, Date"
-etf_table_query = "select T.Name, S.Ticker, S.[Return] as 'OneDayReturn', R.OneWeekReturn as 'OneWeekReturn',\
-      R.OneMonthReturn as 'OneMonthReturn', R.OneQuarterReturn as 'OneQuarterReturn'\
-        , R.OneYearReturn as 'OneYearReturn', S.SigmaSpike, S.KCPos/100 as 'MonthRange', S.[C%52WkRange]/100 as 'YearRange'\
-    from analytics.TodaySnapshot S\
-    left join raw.Tickers T on T.Ticker = S.Ticker \
-    left join analytics.AggReturns R on R.Ticker  = S.Ticker\
-        where S.SecType = 'ETF' AND S.Ticker LIKE 'XL%'\
-            Order by S.Ticker"
-etf_return_query_one_month_query = "select T.ShortName as 'Name', [Date], [Return], [Order] from raw.Prices P\
-    left join config.TickerShortNames T on T.Ticker = P.Ticker\
-        where T.SecType = 'ETF' AND P.Ticker LIKE 'XL%' AND [RETURN] is not null AND [Order] <=20"
-etf_return_query_one_year_query = "select T.ShortName as 'Name', [Date], [Return], [Order] from raw.Prices P\
+sector_prices_query = "select * from visual.CandlestickChart where SecType = 'ETF'"
+sector_table_query = "select * from visual.PerformanceTable where SecType = 'ETF'"
+
+sector_return_query = "select T.ShortName as 'Name', [Date], [Return], [Order] from raw.Prices P\
     left join config.TickerShortNames T on T.Ticker = P.Ticker\
         where T.SecType = 'ETF' AND P.Ticker LIKE 'XL%' AND [RETURN] is not null"
 
-stock_heatmap_query = "select TOP 1000 S.Ticker,S.SecType,S.[Return], COALESCE(S.SigmaSpike, 0) as SigmaSpike, T.Sector, T.Industry, T.MarketCap\
-    from analytics.TodaySnapshot S\
-    left join raw.Tickers T on T.Ticker = S.Ticker \
-    where S.SecType = 'Stock'\
-    order by MarketCap desc"
-
-# summary table settings
-sum_table_rename_source = ['OneDayReturn', 'OneWeekReturn','OneMonthReturn','OneQuarterReturn','OneYearReturn', 'SigmaSpike','MonthRange','YearRange']
-sum_table_rename_target = ['One Day Return', 'One Week Return','One Month Return','One Quarter Return','One Year Return', 'Sigma Spike','Month Range','Year Range']
-
-sum_table_percentage_format_subset = ['One Day Return', 'One Week Return','One Month Return','One Quarter Return','One Year Return', 'Sigma Spike','Month Range','Year Range']
-sum_table_color_format_subset = ['One Day Return', 'One Week Return','One Month Return','One Quarter Return','One Year Return', 'Sigma Spike']
-
-
+stock_heatmap_query = "select * from visual.StockHeatmap"
--- st_pages/sector.py
+++ st_pages/sector.py
@@ -2,17 +2,22 @@ import streamlit as st
 import config
 import plotly.graph_objects as go
 import plotly.express as px
+import re
 
 
-st.set_page_config(layout="wide")
+st.set_page_config(layout="wide", page_title="Sector Analysis", page_icon="📈")
 st.markdown(config.condensed_page_style, unsafe_allow_html=True)
 
 
-etf_prices = config.query(config.etf_prices_query)
+st.subheader("Sector Performance")
 
+charts, sector_summary, sector_correlation = st.tabs(
+    ["Charts", "Summary", "Sector Correlations"]
+)
 
-def create_etf_chart(df):
-    tickers = df['Ticker'].unique()
+with charts:
+    sector_prices = config.query(config.sector_prices_query)
+    tickers = sector_prices["Ticker"].unique()
     num_tickers = len(tickers)
     num_cols = 4
     num_rows = (num_tickers + num_cols - 1) // num_cols
@@ -25,34 +30,43 @@ def create_etf_chart(df):
             if ticker_index < num_tickers:
                 ticker = tickers[ticker_index]
                 with cols[j]:
-                    etf_df = df[df['Ticker'] == ticker].copy()
+                    sector_df = sector_prices[sector_prices["Ticker"] == ticker].copy()
 
-                    etf_df['Return'] = (etf_df['Return']*100).round(2)
-                    return_value =  etf_df['Return'].iloc[-1]
-                    return_str = f"+{return_value}%" if return_value >= 0 else f"{return_value}%"
-                    return_color = config.red if return_value <0 else config.green
+                    sector_df["Return"] = (sector_df["Return"] * 100).round(2)
+                    return_value = sector_df["Return"].iloc[-1]
+                    return_str = (
+                        f"+{return_value}%" if return_value >= 0 else f"{return_value}%"
+                    )
+                    return_color = config.red if return_value < 0 else config.green
 
-                    title = f"{etf_df['Name'].iloc[0]} ({etf_df['Ticker'].iloc[0]})"
-                    
-                    candlestick_chart = go.Figure(data=[go.Candlestick(
-                                            x=etf_df['Date'],
-                                            open = etf_df['Open'],
-                                            high = etf_df['High'],
-                                            low=etf_df['Low'],
-                                            close=etf_df['Close'],
-                                            increasing_line_color = config.green,
-                                            increasing_fillcolor = config.green,
-                                            decreasing_line_color = config.red,
-                                            decreasing_fillcolor = config.red
-                                            )])
+                    title = (
+                        f"{sector_df['Name'].iloc[0]} ({sector_df['Ticker'].iloc[0]})"
+                    )
+
+                    candlestick_chart = go.Figure(
+                        data=[
+                            go.Candlestick(
+                                x=sector_df["Date"],
+                                open=sector_df["Open"],
+                                high=sector_df["High"],
+                                low=sector_df["Low"],
+                                close=sector_df["Close"],
+                                increasing_line_color=config.green,
+                                increasing_fillcolor=config.green,
+                                decreasing_line_color=config.red,
+                                decreasing_fillcolor=config.red,
+                            )
+                        ]
+                    )
                     candlestick_chart.update_layout(
                         margin=dict(t=30, b=30, r=30),
-                        height = 300,
-                        title = title,
-                        xaxis_rangeslider_visible=False)
+                        height=300,
+                        title=title,
+                        xaxis_rangeslider_visible=False,
+                    )
                     candlestick_chart.update_xaxes(
                         rangebreaks=[dict(bounds=["sat", "mon"])]
-                        )
+                    )
                     # Add return_str as text in the top right corner
                     candlestick_chart.add_annotation(
                         x=1,
@@ -60,70 +74,111 @@ def create_etf_chart(df):
                         text=return_str,
                         font=dict(color=return_color, size=15),
                         showarrow=False,
-                        align='right',
-                        xref='paper',
-                        yref='paper'
+                        align="right",
+                        xref="paper",
+                        yref="paper",
                     )
                     st.plotly_chart(candlestick_chart, use_container_width=True)
 
-st.subheader("Sector Performance")
+with sector_summary:
+    sector_summary_table = config.query(config.sector_table_query)
+    sector_summary_table = sector_summary_table.drop(["Updated", "SecType"], axis=1)
+    sector_summary_table["MonthRange"] = sector_summary_table["MonthRange"] / 100
+    sector_summary_table["YearRange"] = sector_summary_table["YearRange"] / 100
 
-charts, etf_summary, etf_correlation = st.tabs(["Charts","Summary","Sector Correlations"])
+    sector_summary_table.columns = [
+        re.sub(r"(?<!^)(?=[A-Z])", config.add_space, column)
+        for column in sector_summary_table.columns.tolist()
+    ]
 
-with charts:
-    create_etf_chart(etf_prices)
-
-with etf_summary:
-    etf_summary_table = config.query(config.etf_table_query)
-    etf_summary_table = etf_summary_table.rename(
-        columns={old_col: new_col for old_col, new_col in zip(config.sum_table_rename_source, config.sum_table_rename_target)}
+    sector_summary_table["Name"] = (
+        sector_summary_table["Name"].str.split("Select").str[0].str.strip()
     )
-    etf_summary_table['Name'] = etf_summary_table['Name'].str.split('Select').str[0].str.strip()
-    etf_summary_table.loc[etf_summary_table['Name'] == 'The Real Estate', 'Name'] = 'Real Estate'
-    st.dataframe(etf_summary_table.style.format({col: '{:.2%}' if col != 'Sigma Spike' else '{:.2}'for col in config.sum_table_percentage_format_subset})
-                 .applymap(config.format_positive_negative_cell_color, subset=config.sum_table_color_format_subset), 
-                hide_index=True,
-                height=(11+1)*35+3,
-                use_container_width=True)
+    sector_summary_table.loc[
+        sector_summary_table["Name"] == "The Real Estate", "Name"
+    ] = "Real Estate"
 
-with etf_correlation:
+    percentage_format_subset = [
+        i for i in sector_summary_table.columns.tolist() if i not in ["Name", "Ticker"]
+    ]
+    color_format_subset = [
+        i
+        for i in sector_summary_table.columns.tolist()
+        if i not in ["Name", "Ticker", "Month Range", "Year Range"]
+    ]
+
+    st.dataframe(
+        sector_summary_table.style.format(
+            {
+                col: "{:.2%}" if col != "Sigma Spike" else "{:.2}"
+                for col in percentage_format_subset
+            }
+        ).applymap(
+            config.format_positive_negative_cell_color, subset=color_format_subset
+        ),
+        hide_index=True,
+        height=(11 + 1) * 35 + 3,
+        use_container_width=True,
+    )
+
+with sector_correlation:
     one_month, one_year = st.columns(2)
+    sector_return = config.query(config.sector_return_query)
+    sector_return["Name"] = sector_return["Name"].str.split("Select").str[0].str.strip()
+    sector_return_one_month = sector_return[sector_return["Order"] <= 20]
+
     with one_month:
-        etf_return = config.query(config.etf_return_query_one_month_query)
-        etf_return['Name'] = etf_return['Name'].str.split('Select').str[0].str.strip()
+        correlation_matrix_data = (
+            sector_return_one_month.pivot(
+                columns="Name", values="Return", index="Order"
+            )
+            .corr()
+            .round(2)
+        )
 
-        correlation_matrix_data = etf_return.pivot(columns='Name', values='Return', index='Order').corr().round(2)
-
-        correlation_matrix_plot = px.imshow(correlation_matrix_data,
-                                            text_auto=True, 
-                                            aspect="auto",
-                                            labels=dict(x="Sector", y="Sector"),
-                                            color_continuous_scale=[config.red, config.darkgrey,config.darkgrey, config.green])
+        correlation_matrix_plot = px.imshow(
+            correlation_matrix_data,
+            text_auto=True,
+            aspect="auto",
+            labels=dict(x="Sector", y="Sector"),
+            color_continuous_scale=[
+                config.red,
+                config.darkgrey,
+                config.darkgrey,
+                config.green,
+            ],
+        )
         correlation_matrix_plot.update_coloraxes(showscale=False)
         correlation_matrix_plot.update_layout(
-            margin=dict(t=30),
-            height = 650,
-            title = 'One Month Correlation')
-        
-        st.plotly_chart(correlation_matrix_plot,use_container_width=True)
+            margin=dict(t=30), height=650, title="One Month Correlation"
+        )
+
+        st.plotly_chart(correlation_matrix_plot, use_container_width=True)
     with one_year:
-        etf_return = config.query(config.etf_return_query_one_year_query)
-        etf_return['Name'] = etf_return['Name'].str.split('Select').str[0].str.strip()
-
-        correlation_matrix_data = etf_return.pivot(columns='Name', values='Return', index='Order').corr().round(2)
-        correlation_matrix_plot = px.imshow(correlation_matrix_data,
-                                            text_auto=True, 
-                                            aspect="auto",
-                                            labels=dict(x="Sector", y="Sector"),
-                                            color_continuous_scale=[config.red, config.darkgrey,config.darkgrey, config.green])
+        correlation_matrix_data = (
+            sector_return.pivot(columns="Name", values="Return", index="Order")
+            .corr()
+            .round(2)
+        )
+        correlation_matrix_plot = px.imshow(
+            correlation_matrix_data,
+            text_auto=True,
+            aspect="auto",
+            labels=dict(x="Sector", y="Sector"),
+            color_continuous_scale=[
+                config.red,
+                config.darkgrey,
+                config.darkgrey,
+                config.green,
+            ],
+        )
         correlation_matrix_plot.update_layout(
-            margin=dict(t=30),
-            height = 650,
-            title = 'One Year Correlation')
+            margin=dict(t=30), height=650, title="One Year Correlation"
+        )
         correlation_matrix_plot.update_coloraxes(showscale=False)
-        
-        st.plotly_chart(correlation_matrix_plot,use_container_width=True)
+
+        st.plotly_chart(correlation_matrix_plot, use_container_width=True)
 
 
 date_timestamp = config.query(config.timestamp_query)["Date"][0]
-st.caption(f"Looking at 11 market sector ETFs. EOD Data as of {date_timestamp}")
+st.caption(f"Looking at 11 US sector ETFs. EOD Data as of {date_timestamp}")
